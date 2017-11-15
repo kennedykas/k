221a8fbf4df4b3c2661ef1613f3f3c9feeb3a841
kennedykas <kennedy0064@gmail.com>
2017-11-15 15:30:49 -0200

    jquery removido

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,47 @@
-// funcao para visualizar imagem
-$(document).ready(function(){
-  $(".image").click(function(){
-   $(this).toggleClass("imgViewer");
-  });
-});
-// funcao para o haburguer menu
-$(document).ready(function() {
-	$(document).delegate('.open', 'click', function(event){
-		$(this).addClass('oppenned');
-		event.stopPropagation();
-	})
-	$(document).delegate('body', 'click', function(event) {
-		$('.open').removeClass('oppenned');
-	})
-	$(document).delegate('.cls', 'click', function(event){
-		$('.open').removeClass('oppenned');
-		event.stopPropagation();
-	});
-});
+      document.addEventListener("DOMContentLoaded", function(event) { 
+        // inicialize the particles
+        Particles.init({
+          responsive: [
+            { breakpoint: 1250, options: { maxParticles: 40, speed: .3 }},
+            { breakpoint: 900,  options: { maxParticles: 30, speed: .1 }},
+            { breakpoint: 600,  options: { maxParticles: 20, speed: .1 }},            
+            { breakpoint: 480,  options: { maxParticles: 15, speed: .1 }} 
+          ],
+	  selector: '.background',
+          color: '#ffffff',
+          connectParticles: true,
+          maxParticles: 70,
+          speed: .5,
+          sizeVariations: 4
+        });
+
+
+        // haburguer menu trigger
+        document.getElementById('menuToggler').addEventListener('click', function(event){
+          toggleClass(this, 'oppenned');
+          event.stopPropagation();
+        });
+        // on body click close the menu
+        document.body.addEventListener('click', function(event) {
+          removeClass(document.getElementById('menuToggler'), 'oppenned');
+        })
+
+        // inicialize the image viewer trigger
+        var imgs = document.getElementsByClassName('image');
+
+        for(var i = 0; i < imgs.length; i++) {
+          imgs[i].addEventListener('click', function(event) {
+            toggleClass(this, 'imgViewer');
+            event.stopPropagation();
+          });
+        }
+      });
+
+      // toggle class
+      function toggleClass(element, _class) {
+        element.classList.toggle(_class);
+      }
+      // remove class
+      function removeClass(element, _class) { 
+        element.classList.remove(_class);
+      }
