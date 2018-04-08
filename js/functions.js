25147e86d04b39d2d38bcc6819c7430842bb9842
kennedykas <kennedy0064@gmail.com>
2018-04-08 13:30:31 -0300

    08/04/2018 updates

--- js/functions.js
+++ js/functions.js
@@ -1,47 +1,46 @@
-      document.addEventListener("DOMContentLoaded", function(event) { 
-        // inicialize the particles
-        Particles.init({
-          responsive: [
-            { breakpoint: 1250, options: { maxParticles: 40, speed: .3 }},
+window.onload = function() {
+
+    Particles.init({
+        responsive: [
+            // { breakpoint: 1250, options: { maxParticles: 40 }},
             { breakpoint: 900,  options: { maxParticles: 30, speed: .1 }},
-            { breakpoint: 600,  options: { maxParticles: 20, speed: .1 }},            
-            { breakpoint: 480,  options: { maxParticles: 15, speed: .1 }} 
-          ],
-	  selector: '.background',
-          color: '#ffffff',
-          connectParticles: true,
-          maxParticles: 70,
-          speed: .5,
-          sizeVariations: 4
-        });
+            { breakpoint: 600,  options: { maxParticles: 20, speed: .1 }},
+            { breakpoint: 480,  options: { maxParticles: 15, speed: .1 }}
+        ],
+        selector        : '.background',
+        color           : '#ffffff',
+        connectParticles: true,
+        maxParticles    : 40,
+        speed           : .2,
+        sizeVariations  : 4
+    });
+};
 
+// haburguer menu trigger
+document.getElementById('menuToggler').addEventListener('click', function(event){
+  toggleClass(this, 'oppenned');
+  event.stopPropagation();
+});
+// on body click close the menu
+document.body.addEventListener('click', function(event) {
+  removeClass(document.getElementById('menuToggler'), 'oppenned');
+});
 
-        // haburguer menu trigger
-        document.getElementById('menuToggler').addEventListener('click', function(event){
-          toggleClass(this, 'oppenned');
-          event.stopPropagation();
-        });
-        // on body click close the menu
-        document.body.addEventListener('click', function(event) {
-          removeClass(document.getElementById('menuToggler'), 'oppenned');
-        })
+// inicialize the image viewer trigger
+var imgs = document.getElementsByClassName('image');
 
-        // inicialize the image viewer trigger
-        var imgs = document.getElementsByClassName('image');
+for(var i = 0; i < imgs.length; i++) {
+  imgs[i].addEventListener('click', function(event) {
+    toggleClass(this, 'imgViewer');
+    event.stopPropagation();
+  });
+}
 
-        for(var i = 0; i < imgs.length; i++) {
-          imgs[i].addEventListener('click', function(event) {
-            toggleClass(this, 'imgViewer');
-            event.stopPropagation();
-          });
-        }
-      });
-
-      // toggle class
-      function toggleClass(element, _class) {
-        element.classList.toggle(_class);
-      }
-      // remove class
-      function removeClass(element, _class) { 
-        element.classList.remove(_class);
-      }
+// toggle class
+function toggleClass(element, _class) {
+    element.classList.toggle(_class);
+}
+// remove class
+function removeClass(element, _class) {
+    element.classList.remove(_class);
+}
